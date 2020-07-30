0c8d01abfb07ddca53470b3ed68410845023cc92
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-30 17:18:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,12 +5,13 @@ class Anagram
     @word = word 
   end   
   def match(list)
-   list.select {|x| x.split("").sort == @word.split("").sort}
-    #list.each do |element|
-      #(@word.split("").sort) == (element.split("").sort)
-        #return_array << element
-      #end
-    #end
-    #return_array
+   #list.select {|x| x.split("").sort == @word.split("").sort}
+    return_array = []
+    list.each do |element|
+      @word.split("").sort) == (element.split("").sort
+        return_array << element
+      end
+    end
+    return_array
 end
 end
