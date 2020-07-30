beddfa889d9af874214549693b3b49a463dcebef
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-30 17:15:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,10 @@
+
 class Anagram
   attr_accessor :word
-  def initialize (word)
-    @word=word
-  end
-  def match (list)
-    split_sort_word=@word.split("").sort
-    list.select do |word|
-    word.split("").sort == split_sort_word
-  end
+  def initialize(word)
+    @word = word 
+  end   
+  def match(list)
+   list.select {|x| x.split("").sort == @word.split("").sort}
 end
 end
